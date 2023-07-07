ec46b3ad7b4b099adc976488fb11fc3eaa8eb21b
lcscarv <carvalholucas01@hotmail.com>
2023-07-07 11:14:43 -0300

    Addition of person_id in both model and documentation files

--- models/staging/stg_creditcard.sql
+++ models/staging/stg_creditcard.sql
@@ -6,8 +6,25 @@ WITH credit_card AS (
          , expyear as card_exp_year
          
     FROM {{source('raw_sheets','creditcard')}}
-    
-    
+    )
+    , person_credit_card AS (
+        SELECT 
+            businessentityid as person_id
+             , creditcardid as credit_card_id
+
+        FROM {{source('raw_sheets','personcreditcard')}}
     )
 
-SELECT * FROM credit_card
+    , join_all AS (
+        SELECT 
+            credit_card.credit_card_id
+             , person_credit_card.person_id
+             , credit_card.card_type
+             , credit_card.card_exp_month
+             , credit_card.card_exp_year
+        FROM credit_card
+        LEFT JOIN person_credit_card
+            ON person_credit_card.credit_card_id = credit_card.credit_card_id
+    )
+
+SELECT * FROM join_all
