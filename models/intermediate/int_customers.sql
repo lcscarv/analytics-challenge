ed137e30791a359de73eef98856fb73e86d462fb
lcscarv <carvalholucas01@hotmail.com>
2023-07-07 11:00:38 -0300

    Update on credit Card type to avoid null values

--- models/intermediate/int_customers.sql
+++ models/intermediate/int_customers.sql
@@ -7,7 +7,7 @@ WITH
     , person AS (
         SELECT 
         business_entity_id as person_id
-         , person_first_name || COALESCE(person_middle_name,'') || ' ' || person_last_name || COALESCE(person_suffix,'') as customer_name
+         , person_first_name || ' ' ||COALESCE(person_middle_name,'') || ' ' || person_last_name || COALESCE(person_suffix,'') as customer_name
         FROM {{ref('stg_person')}}   
 )
 
@@ -25,6 +25,7 @@ WITH
         SELECT 
         state_province_id
          , territory_id
+         , state_name
          , country_region_code
 
         FROM {{ref('stg_stateprovince')}}
@@ -42,6 +43,7 @@ WITH
     , credit_card AS (
         SELECT
         credit_card_id
+         , person_id
          , card_type
         
         FROM {{ref('stg_creditcard')}}
@@ -66,27 +68,32 @@ WITH
          , customer.territory_id
          , person.customer_name
          , sales_header.address_id
-         , sales_header.credit_card_id
-          , credit_card.card_type
+         , credit_card.credit_card_id
+         , CASE 
+            WHEN credit_card.card_type IS NULL
+                THEN 'Not Informed'
+            ELSE credit_card.card_type
+        END AS card_type
          , customer_address.state_province_id
          , customer_address.customer_city
          , customer_address.customer_address
          , customer_state.country_region_code
+         , customer_state.state_name as customer_state
          , customer_country.country_name as customer_country
     
         FROM customer
 
         INNER JOIN person
             ON person.person_id = customer.person_id        
-        INNER JOIN sales_header
+        LEFT JOIN sales_header
             ON sales_header.customer_id = customer.customer_id
-        INNER JOIN customer_address 
-            ON customer_address.address_id = sales_header.address_id
-        INNER JOIN credit_card
-            ON credit_card.credit_card_id = sales_header.credit_card_id
-        INNER JOIN customer_state
-            ON customer_state.state_province_id = customer_address.state_province_id
-        INNER JOIN customer_country
+        LEFT JOIN customer_address 
+            ON  sales_header.address_id = customer_address.address_id
+        LEFT JOIN credit_card
+            ON customer.person_id = credit_card.person_id 
+        LEFT JOIN customer_state
+            ON  customer_address.state_province_id = customer_state.state_province_id 
+        LEFT JOIN customer_country
             ON customer_country.country_region_code = customer_state.country_region_code
     )
         
