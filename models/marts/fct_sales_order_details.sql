84ce8b9c3c2063eea0989f8b561bbbb2310e5fd0
lcscarv <carvalholucas01@hotmail.com>
2023-07-07 11:30:47 -0300

    Staging model now contains net income and gross revenue. Fact table only access it. Documentation updated

--- models/marts/fct_sales_order_details.sql
+++ models/marts/fct_sales_order_details.sql
@@ -7,8 +7,8 @@ WITH
          , order_qty
          , unit_price
          , unit_price_discount       
-         , (unit_price*order_qty) as gross_revenue
-         , ((1 - unit_price_discount)*unit_price*order_qty) as net_income
+         , gross_revenue
+         , net_income
          
         FROM {{ref('stg_sales_order_dtl')}}
         )
--- models/staging/stg_sales_order_dtl.sql
+++ models/staging/stg_sales_order_dtl.sql
@@ -6,6 +6,8 @@ WITH sales_order_detail AS (
      , orderqty as order_qty
      , unitprice as unit_price
      , unitpricediscount as unit_price_discount
+     , (unit_price*order_qty) as gross_revenue
+     , ((1 - unit_price_discount)*unit_price*order_qty) as net_income
     
     FROM {{source('raw_sheets','salesorderdetail')}}
     )
