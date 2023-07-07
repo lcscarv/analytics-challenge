7cc02e434dc8415d2bc78c6d47acee62dd914a59
lcscarv <carvalholucas01@hotmail.com>
2023-07-07 11:10:52 -0300

    Addition of state name in both model and documentation files

--- models/staging/stg_stateprovince.sql
+++ models/staging/stg_stateprovince.sql
@@ -2,6 +2,7 @@ WITH state_province AS (
     SELECT
     stateprovinceid as state_province_id
      , territoryid as territory_id
+     , name as state_name
      , stateprovincecode as state_province_code
      , countryregioncode as country_region_code
      , isonlystateprovinceflag as state_prov_code_availability
